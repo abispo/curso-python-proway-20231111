b47788c9d1573ec63f7ab3134663d7f9a4e98b0e
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-11-18 14:43:48 -0300

    Arquivos aula 02 Python fundamentos

---
+++ modulo01-fundamentos/20231118-aula02/prog01.py
@@ -0,0 +1,26 @@
+"""
+Estruturas de condição
+
+match case
+
+O match case é a segunda estrutura de controle de condição no Python. Foi introduzida a partir da versão 3.10 da linguagem. Basicamente é o correspondente em Python do switch case, de linguagens como C# e Java.
+
+É recomendado utilizar o match case em comparações mais simples, onde sabemos exatamente a quantidade de opções que podemos processar. Já o if utilizamos em comparações mais complexas.
+"""
+
+if __name__ == "__main__":
+
+    nivel_de_acesso = int(input("Informe o nivel de acesso: "))
+
+    match nivel_de_acesso:
+        case 1:
+            print("Seu nível de acesso é de administrador.")
+
+        case 2:
+            print("Seu nível de acesso é de moderador.")
+
+        case 3:
+            print("Seu nível de acesso é de usuário.")
+
+        case _:
+            print("Nível de acesso desconhecido.")
