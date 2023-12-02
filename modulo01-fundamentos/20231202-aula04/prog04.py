8b3da3ee31fcdd033f139d28c48af33c4e953898
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2023-12-02 17:41:18 -0300

    Arquivos aula 04 python fundamentos

--- modulo01-fundamentos/20231202-aula04/prog04.py
+++ modulo01-fundamentos/20231202-aula04/prog04.py
@@ -29,3 +29,28 @@ if __name__ == "__main__":
 
         # O método writerows recebe uma lista de listas
         arquivo_csv.writerows(carrinho_de_compras)
+
+    # ------------------------------------------------
+
+    # Trabalhando com o DictWriter
+    carrinho_de_compras = [
+        {"id": 1, "nome": "Pilha AAA", "quantidade": 2, "valor_unitario": 19.90},
+        {"id": 2, "nome": "Fone de ouvido", "quantidade": 1, "valor_unitario": 56.40},
+        {"id": 3, "nome": "Teclado Gamer", "quantidade": 1, "valor_unitario": 111.90},
+    ]
+
+    # Criamos o arquivo
+    nome_arquivo = os.path.join(os.getcwd(), "arquivos", "compras.csv")
+    with open(nome_arquivo, mode='w', encoding='utf-8', newline="") as arquivo:
+
+        # Definimos a linha de cabeçalho do arquivo csv
+        fieldnames = ["id", "nome", "quantidade", "valor_unitario"]
+
+        # Quando instanciamos a classe DictWriter, indicamos o cabeçalho anterior em fieldnames
+        arquivo_csv = csv.DictWriter(arquivo, delimiter=';', fieldnames=fieldnames)
+
+        # O método writeheader() escreve no arquivo csv o cabeçalho, ou seja, os valores de fieldnames
+        arquivo_csv.writeheader()
+
+        # Escreve o conteúdo no arquivo recebendo a lista de dicionários
+        arquivo_csv.writerows(carrinho_de_compras)
