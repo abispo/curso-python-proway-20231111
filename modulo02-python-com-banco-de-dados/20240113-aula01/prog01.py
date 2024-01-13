8a4af404dc58e03415dbed2f764c42e5a856a6e1
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-01-13 15:07:24 -0300

    Arquivos aula 01 python com banco de dados

--- modulo02-python-com-banco-de-dados/20240113-aula01/prog01.py
+++ modulo02-python-com-banco-de-dados/20240113-aula01/prog01.py
@@ -27,4 +27,60 @@ conexao = sqlite3.connect(connection_string)
 cursor = conexao.cursor()
 
 if __name__ == "__main__":
-    pass
+    
+    comando = """
+
+    CREATE TABLE IF NOT EXISTS tb_estados(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        uf TEXT NOT NULL
+    );
+    """
+
+    # Executando o comando definido acima
+    cursor.execute(comando)
+
+    # Inserindo dados na tabela
+    lista_estados = [
+        {"nome": "Acre", "sigla": "AC"},
+        {"nome": "Alagoas", "sigla": "AL"},
+        {"nome": "Amapá", "sigla": "AP"},
+        {"nome": "Amazonas", "sigla": "AM"},
+        {"nome": "Bahia", "sigla": "BA"},
+        {"nome": "Ceará", "sigla": "CE"},
+        {"nome": "Distrito Federal", "sigla": "DF"},
+        {"nome": "Espírito Santo", "sigla": "ES"},
+        {"nome": "Goiás", "sigla": "GO"},
+        {"nome": "Maranhão", "sigla": "MA"},
+        {"nome": "Mato Grosso", "sigla": "MT"},
+        {"nome": "Mato Grosso do Sul", "sigla": "MS"},
+        {"nome": "Minas Gerais", "sigla": "MG"},
+        {"nome": "Pará", "sigla": "PA"},
+        {"nome": "Paraíba", "sigla": "PB"},
+        {"nome": "Paraná", "sigla": "PR"},
+        {"nome": "Pernambuco", "sigla": "PE"},
+        {"nome": "Piauí", "sigla": "PI"},
+        {"nome": "Rio de Janeiro", "sigla": "RJ"},
+        {"nome": "Rio Grande do Norte", "sigla": "RN"},
+        {"nome": "Rio Grande do Sul", "sigla": "RS"},
+        {"nome": "Rondônia", "sigla": "RO"},
+        {"nome": "Roraima", "sigla": "RR"},
+        {"nome": "Santa Catarina", "sigla": "SC"},
+        {"nome": "São Paulo", "sigla": "SP"},
+        {"nome": "Sergipe", "sigla": "SE"},
+        {"nome": "Tocantins", "sigla": "TO"}
+    ]
+
+    for estado in lista_estados:
+
+        nome = estado.get("nome")
+        uf = estado.get("sigla")
+
+        # Montar o comando de inserção de dados
+        comando = "INSERT INTO tb_estados (nome, uf) VALUES ('{}', '{}');".format(
+            nome, uf
+        )
+
+        cursor.execute(comando)
+
+        conexao.commit()
