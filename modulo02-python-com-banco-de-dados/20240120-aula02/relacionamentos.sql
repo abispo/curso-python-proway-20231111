12722e6ab76ed18fffbc150ccc5c4237a71d3856
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-01-20 17:50:16 -0300

    Arquivos aula 02 python com banco de dados

--- modulo02-python-com-banco-de-dados/20240120-aula02/relacionamentos.sql
+++ modulo02-python-com-banco-de-dados/20240120-aula02/relacionamentos.sql
@@ -45,4 +45,52 @@ INSERT INTO tb_perfis (id, nome, data_nascimento) VALUES
 	(3, "José Anchieta", "1958-12-05");
 SELECT * FROM tb_perfis tp ;
 
+# A linha abaixo dará erro, pois já existe um registro com a chave primária igual a 1
 INSERT INTO tb_perfis (id, nome, data_nascimento) VALUES (1, "Teste", "1900-01-01");
+
+/*
+No caso acima, criamos uma relação de Um para Um (1:1) entre a tabela tb_usuarios e a tabela
+tb_perfis, pois a coluna chave primária da tabela tb_perfis, também é chave estrangeira
+que aponta para a tabela tb_usuarios. Dessa maneira, nenhum usuário terá mais de 1 perfil
+na tabela de perfis.
+
+*/
+
+# Exemplo: Mostrar email, nome e data de nascimento do usuário
+SELECT tu.email, tp.nome, tp.data_nascimento FROM tb_usuarios tu 
+INNER JOIN tb_perfis tp 
+ON tu.id = tp.id;
+
+-- ///// --
+
+# Criar a tabela de postagens
+CREATE TABLE IF NOT EXISTS tb_postagens(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	usuario_id INT NOT NULL,
+	titulo VARCHAR(200) NOT NULL,
+	corpo TEXT NOT NULL,
+	FOREIGN KEY (usuario_id) REFERENCES tb_usuarios(id)
+);
+
+# Inserir registros
+INSERT INTO tb_postagens (usuario_id, titulo, corpo) VALUES
+	(1, "Python é legal", "Python é uma linguagem bem legal."),
+	(1, "Java é poderoso", "Java tem muitos recursos."),
+	(2, "Linux é maneiro", "Estou aprendendo Linux para ser devops.");
+SELECT * FROM tb_postagens tp ;
+
+# No caso acima, a regra de negócio é a seguinte: Um usuário pode fazer diversas postagens,
+# porém uma postagem é feita apenas por 1 usuário. Nesse cenário temos uma relação de
+# Um para Muitos (1:N) entre as tabelas tb_usuarios e tb_postagens. Nas relações 1:N, a
+# chave estrangeira ficará sempre na tabela dependente (a tabela que fica do lado N da relação).
+
+# Exemplo: Mostrando os posts feitos por um usuário
+
+SELECT tp.nome, tu.email, tpo.titulo FROM tb_perfis tp 
+INNER JOIN tb_usuarios tu 
+ON tp.id = tu.id
+INNER JOIN tb_postagens tpo
+ON tu.id = tpo.usuario_id 
+WHERE tpo.usuario_id = 1;
+
+-- ///// --
