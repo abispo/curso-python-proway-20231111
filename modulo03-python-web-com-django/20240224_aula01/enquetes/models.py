65b06985f2ce38915618874a9645e861a1555397
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-02-24 17:04:21 -0300

    arquivos aula 01 python web

--- modulo03-python-web-com-django/20240224_aula01/enquetes/models.py
+++ modulo03-python-web-com-django/20240224_aula01/enquetes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField(max_length=200)
+    data_de_publicacao = models.DateTimeField("Data de publicação")
+
+    class Meta:
+        db_table = "tb_perguntas"
+
+
+class Opcao(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    texto = models.CharField(max_length=100)
+    votos = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "tb_opcoes"
--- modulo03-python-web-com-django/20240224_aula01/site_pessoal/urls.py
+++ modulo03-python-web-com-django/20240224_aula01/site_pessoal/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path("__debug__/", include("debug_toolbar.urls")),
+
     path('enquetes/', include('enquetes.urls'))
 ]
