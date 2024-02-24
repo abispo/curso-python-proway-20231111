c4ef32275110b060cb6005bc1f6c760afa6fe862
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-02-24 18:06:49 -0300

    Arquivos aula 01 python web

--- modulo03-python-web-com-django/20240224_aula01/enquetes/models.py
+++ modulo03-python-web-com-django/20240224_aula01/enquetes/models.py
@@ -5,6 +5,9 @@ class Pergunta(models.Model):
     texto = models.CharField(max_length=200)
     data_de_publicacao = models.DateTimeField("Data de publicação")
 
+    def __str__(self):
+        return self.texto
+
     class Meta:
         db_table = "tb_perguntas"
 
@@ -13,6 +16,9 @@ class Opcao(models.Model):
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     texto = models.CharField(max_length=100)
     votos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.texto} ({self.pergunta.texto})"
 
     class Meta:
         db_table = "tb_opcoes"
