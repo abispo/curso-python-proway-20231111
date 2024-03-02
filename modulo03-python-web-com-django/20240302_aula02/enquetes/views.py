2201ac0a638dde2af46ca198ed7c0a3396758f88
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-03-02 18:13:00 -0300

    Arquivos aula 02 Python web

--- modulo03-python-web-com-django/20240302_aula02/enquetes/views.py
+++ modulo03-python-web-com-django/20240302_aula02/enquetes/views.py
@@ -2,9 +2,10 @@
 # A classe HttpResponse é utilizada quando queremos retornar uma resposta HTTP válida
 from django.http.response import HttpResponse
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 
-from enquetes.models import Pergunta
+from enquetes.models import Pergunta, Opcao
 
 """
 As funções criadas dentro do módulo views, devem ser consideradas views que serão ligadas a uma url, criando uma rota
@@ -35,7 +36,35 @@ def detalhe(request, pergunta_id):
     )
 
 def resultados(request, pergunta_id):
-    return HttpResponse(f"Você está nos resultados da pergunta {pergunta_id}")
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    return render(
+        request,
+        "enquetes/resultados.html",
+        {"pergunta": pergunta}
+    )
 
 def votar(request, pergunta_id):
-    return HttpResponse(f"Você está votando na pergunta {pergunta_id}")
+    
+    pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+    try:
+        opcao_escolhida = pergunta.opcao_set.get(pk=request.POST["opcao"])
+        
+    except (KeyError, Opcao.DoesNotExist):
+        return render(
+            request,
+            "enquetes/detalhe.html",
+            {
+                "mensagem_erro": "Você não escolheu uma opção.",
+                "pergunta": pergunta
+            }
+        )
+    
+    else:
+        opcao_escolhida.votos += 1
+        opcao_escolhida.save()
+
+        return redirect(
+            reverse("enquetes:resultados", args=(pergunta.id,))
+        )
