8a81009c544c677091e749da99ecb00a7a5193cf
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-03-09 18:01:46 -0300

    Arquivos aula 03 python web

--- modulo03-python-web-com-django/20240309_aula03/site_pessoal/settings.py
+++ modulo03-python-web-com-django/20240309_aula03/site_pessoal/settings.py
@@ -13,15 +13,18 @@ import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k&5xpx4r&u6uj(-x^ed42kdqsmndf3504x@4vi2cbruai%dvx('
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
