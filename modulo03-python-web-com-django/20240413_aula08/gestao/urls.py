413d7ec532b6344e3c6d8e3f4357fd1fafd27b4b
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-04-13 16:20:20 -0300

    Arquivos aula 08 python web

--- modulo03-python-web-com-django/20240413_aula08/gestao/urls.py
+++ modulo03-python-web-com-django/20240413_aula08/gestao/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = "gestao"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path(
+        "inquilinos/<int:id_do_usuario>/perfil/",
+        views.perfil_inquilino,
+        name="perfil_inquilino"
+    )
 ]
--- modulo03-python-web-com-django/20240413_aula08/gestao/views.py
+++ modulo03-python-web-com-django/20240413_aula08/gestao/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 def index(request):
@@ -5,3 +6,10 @@ def index(request):
         request,
         "gestao/index.html"
     )
+
+def perfil_inquilino(request: HttpRequest, id_do_usuario: int):
+
+    if request.method == "GET":
+        return render(
+            request, "inquilinos/perfil_inquilino.html"
+        )
