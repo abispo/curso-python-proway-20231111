377f8f2538d20dfb1c3d0d140bd1459b239cfd18
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2024-04-20 14:18:19 -0300

    Arquivos aula 09 python web

--- modulo03-python-web-com-django/20240420_aula09/gestao/models.py
+++ modulo03-python-web-com-django/20240420_aula09/gestao/models.py
@@ -1,3 +1,22 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+class Imovel(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    tipo_logradouro = models.CharField(max_length=20)
+    logradouro = models.CharField(max_length=200)
+    numero = models.CharField(max_length=20)
+    complemento = models.CharField(max_length=20)
+    bairro = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2)
+    disponivel = models.BooleanField(default=True)
+    
+    class Meta:
+        db_table = "tb_imoveis"
